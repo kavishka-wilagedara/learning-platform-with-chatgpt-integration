4153f6bf3986b44138661e4f415377550192330c
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 09:54:34 +0530

    feat(course): add course registration validator

---
+++ backend/src/utils/validators/courseValidator.js
@@ -0,0 +1,16 @@
+const validateCourseCreate = ({ title, description}) => {
+    if(!title || title.trim() === ""){
+        return "Title is required"
+    }
+
+    if(!description || description.trim() === ""){
+        return "Description is required"
+    }
+
+    return null
+}
+
+module.exports = {
+    validateCourseCreate
+}
+
