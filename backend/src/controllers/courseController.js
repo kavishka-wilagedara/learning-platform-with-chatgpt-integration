42918d656985f6c789c64d1164339adbf74ae12d
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 12:46:14 +0530

    feat(course): add getAllPublished courses feature

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1,30 +1,32 @@
 const Course = require("../models/course")
 const {validateCourseCreate} = require("../utils/validators/courseValidator")
 
-
 const createCourse = async(req, res) => {
 
     try{
         // Validate course inputs
         const error = validateCourseCreate(req.body)
         if(error){
-            console.error('error', error)
-            return res.status(400).json({message: error})
+            return res.status(400).json({
+                success: false,
+                message: error
+            })
         }
 
-        const { title, description, instructor, content, isPublished } = req.body
+        const { title, description, content, isPublished } = req.body
+        const instructor = req.user.id
 
-        const newCourse = new Course({
+        const newCourse = await Course.create({
             title,
             description,
             instructor,
             content,
-            isPublished: isPublished || true
+            isPublished: isPublished ?? true
         })
-        await newCourse.save()
-        console.log(`Course created successfully. ID: ${newCourse._id}, Title: ${newCourse.title}`)
+        console.log(`Course created successfully | ID: ${newCourse._id}, Title: ${newCourse.title}`)
 
         res.status(201).json({
+            success: true,
             message: 'Course created successfully!',
             course: {
                 id: newCourse._id,
@@ -36,10 +38,38 @@ const createCourse = async(req, res) => {
             }
         })
     }
+
     catch(error){
         console.error('Course registation failed: ', error)
-        res.status(500).json({message: 'Course registration failed!'})
+
+        res.status(500).json({
+            success: false,
+            message: 'Course registration failed!'})
     }
 }
 
-module.exports = {createCourse}
+const getAllPublishedCourses = async(req, res) => {
+    try {
+    const courses = await Course.find({isPublished: true})
+        .select('title description instructor content')
+        .populate('instructor', 'firstname lastname')
+        .lean()
+
+    res.status(200).json({
+        success: true,
+        count: courses.length,
+        data: courses
+    });
+
+  } catch (error) { 
+    console.error('Course fetching failed: ', error);
+
+    res.status(500).json({ 
+        success: false,
+        message: 'Failed to fetch courses' }); }
+}
+
+module.exports = {
+    createCourse,
+    getAllPublishedCourses
+}
--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const { createCourse } = require("../controllers/courseController")
+const { getAllPublishedCourses } = require("../controllers/courseController")
 const router = express.Router()
 
 router.post("/create", createCourse)
+router.get("/all", getAllPublishedCourses)
 
 module.exports = router
