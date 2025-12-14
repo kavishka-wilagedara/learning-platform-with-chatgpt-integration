92b28bd13480471983924125d1d76645fe849cf8
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 13:22:23 +0530

    feat(user): add role validator before user registration

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,15 +1,16 @@
 const bcrypt = require("bcryptjs")
 const User = require("../models/user")
 const generateToken = require("../utils/generateToken")
-const {validateRegister} = require("../utils/validators/userValidator")
+const {validateRegister, roleValidate} = require("../utils/validators/userValidator") 
 
 const register =  async(req, res) => {
     try{
         // Validate user inputs
         const error = validateRegister(req.body)
             if (error) {
-                console.error('error', error)
-                return res.status(400).json({ message: error })
+                return res.status(400).json({ 
+                    success: false,
+                    message: error })
             }
 
         const {firstname, lastname, username, password, role} = req.body
@@ -17,26 +18,31 @@ const register =  async(req, res) => {
         // Username validation
         const existingUser = await User.findOne({ username })
         if(existingUser){
-            return res.status(400).json({message: 'Username already in use!'})
+            return res.status(400).json({
+                success: false,
+                message: 'Username already in use!'})
         }
+
+        // Role validate and assigning
+        const assignedRole = roleValidate(req.body);
 
         // Password hashing
         const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = new User({
+        const newUser = await User.create({
             firstname,
             lastname, 
             username,
             password: hashedPassword,
-            role: role || 'student'
+            role: assignedRole
         })
-        await newUser.save()
-        console.log(`User registered successfully. ID: ${newUser._id}, Role: ${newUser.role}`)
+        console.log(`User registered successfully | ID: ${newUser._id}, Role: ${newUser.role}`)
 
         // Generate the token using the id and role
         const token = generateToken(newUser._id, newUser.role)
 
         console.log('Sending success response to client.')
         res.status(201).json({
+            success: true,
             token,
             user: {
                 id: newUser._id,
@@ -49,7 +55,10 @@ const register =  async(req, res) => {
     }
     catch(error){
         console.error('User registation failed: ', error)
-        res.status(500).json({message: 'User registration failed!'})
+
+        res.status(500).json({
+            success: false,
+            message: 'User registration failed!'})
     }
 
 }
@@ -57,22 +66,28 @@ const register =  async(req, res) => {
 const login = async(req, res) => {
     try{
         const {username, password} = req.body;
+
+        // Find username
         const user = await User.findOne({username})
-
         if(!user){
-            return res.status(404).json({message: 'Invalid credential!'})
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credential!'})
         }
 
+        // Compare password
         const isMatchPassword = await bcrypt.compare(password, user.password)
-
         if(!isMatchPassword){
-            return res.status(400).json({message: 'Invalid password!'})
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credential!'})
         }
 
-        // Generate the token using the id and role
+        // Generate the token
         const token = generateToken(user._id, user.role)
 
         res.status(200).json({
+            success: true,
             token,
             user: {
                 id: user._id,
@@ -86,7 +101,10 @@ const login = async(req, res) => {
 
     }catch(error){
         console.error("Login failed: ", error)
-        res.status(500).json({message: 'Login failed!'})
+
+        res.status(500).json({
+            success: false,
+            message: 'Login failed!'})
     }    
 }
 
--- backend/src/utils/validators/userValidator.js
+++ backend/src/utils/validators/userValidator.js
@@ -1,27 +1,34 @@
 const validateRegister = ({ firstname, lastname, username, password}) => {
     if (!firstname || firstname.trim() === "") {
-        return "First name is required"
+        return "First name is required";
     }
 
     if (!lastname || lastname.trim() === "") {
-        return "Last name is required"
+        return "Last name is required";
     }
 
     if (!username || username.trim() === "") {
-        return "Username is required"
+        return "Username is required";
     }
 
     if (!password || password.trim() === "") {
-        return "Password is required"
+        return "Password is required";
     }
 
     if (password.trim().length < 8) {
-        return "Password must be at least 8 characters"
+        return "Password must be at least 8 characters";
     }
 
-    return null
+    return null;
+}
+
+const roleValidate = ({role}) => {
+    const allowedRoles = ['student', 'instructor'];
+    const assignedRole = allowedRoles.includes(role) ? role: 'student';
+    return assignedRole;
 }
 
 module.exports = {
-  validateRegister
+  validateRegister,
+  roleValidate
 }
