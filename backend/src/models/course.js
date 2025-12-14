a65b40442b9c87e49291ba4d78bc1da6ba62aedf
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 09:49:18 +0530

    feat(course): add course schema

--- backend/src/models/course.js
+++ backend/src/models/course.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose")
 
-const courseSchema = new mongoose.Schema({
-    
-})
+const courseSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        instructor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        content: {
+            type: String
+        },
+        isPublished: {
+        type: Boolean,
+        default: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model("Course", courseSchema)
