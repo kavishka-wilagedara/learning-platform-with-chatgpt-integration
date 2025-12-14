f55ac3a3674e633a5b7b25c805dfcdb7b83ab46b
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 18:51:34 +0530

    feat: return separate errors for course create and update

--- backend/src/utils/validators/courseValidator.js
+++ backend/src/utils/validators/courseValidator.js
@@ -1,14 +1,20 @@
-const validateCourseCreateOrUpdate = ({ title, description}) => {
-    if(!title || title.trim() === ""){
-        return "Title is required"
-    }
+const validateCourseCreateOrUpdate = (data, isUpdate = false) => {
+ if (!isUpdate) {
+    if (!data.title || data.title.trim() === "") 
+        return "Title is required";
+    if (!data.description || data.description.trim() === "") 
+        return "Description is required";
+  } 
+  
+  else {
+    if (data.title !== undefined && data.title.trim() === "") 
+        return "Title cannot be empty";
+    if (data.description !== undefined && data.description.trim() === "") 
+        return "Description cannot be empty";
+  }
 
-    if(!description || description.trim() === ""){
-        return "Description is required"
-    }
-
-    return null
-}
+  return null;
+};
 
 module.exports = {
     validateCourseCreateOrUpdate
