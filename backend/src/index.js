781b0c454e951f584c8c52f2b203a35bd7e3e7d6
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 23:58:34 +0530

    add enrollment routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config()
 const dbConnect = require("./config/dbConnection")
 const authRoutes = require("./routes/authRoutes")
 const courseRoutes = require("./routes/courseRoutes")
+const enrollmentRoutes = require("./routes/enrollmentRoutes")
 
 dbConnect()
 
@@ -16,6 +17,9 @@ app.use("/api/auth", authRoutes)
 
 // Course routes
 app.use("/api/course", courseRoutes)
+
+// Enrollment routes
+app.use("/api/enrollment", enrollmentRoutes)
 
 // Start the server
 const PORT = process.env.PORT || 7002
