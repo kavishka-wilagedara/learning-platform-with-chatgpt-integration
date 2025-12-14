bdb14aef947b76d052020ca94470f36959377819
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-14 15:46:51 +0530

    feat(auth): add RBAC to protect routes

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -2,9 +2,20 @@ const express = require("express")
 const { createCourse } = require("../controllers/courseController")
 const { getAllPublishedCourses } = require("../controllers/courseController")
 const router = express.Router()
-const {verifyToken} = require("../middlewares/authMiddleware")
+const { verifyToken } = require("../middlewares/authMiddleware")
+const { authorizeRoles } = require("../middlewares/roleMiddleware")
 
-router.post("/create", verifyToken, createCourse)
-router.get("/publish/all", verifyToken, getAllPublishedCourses)
+router.post(
+    "/create",
+    verifyToken,
+    authorizeRoles('instructor'),
+    createCourse
+);
+
+router.get("/publish/all",
+    verifyToken,
+    authorizeRoles('student'),   // Only students visible all published courses
+    getAllPublishedCourses
+);
 
 module.exports = router
