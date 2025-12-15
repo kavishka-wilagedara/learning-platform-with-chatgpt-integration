62877cd364fb90184c81836ead4957599a66575b
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 04:53:06 +0530

    add user register route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Register from "./pages/Register";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 
       <Route path="/" element= {<Home />}/>
       <Route path="/login" element= {<Login />}/>
+      <Route path="/register" element= {<Register />}/>
 
       </Routes>
     </Router>
