028a05c3083ab8156eec42199682d68cb42cf29e
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 03:59:46 +0530

    use cors to allow all origins for testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const dotenv = require("dotenv").config()
 const dbConnect = require("./config/dbConnection")
 const authRoutes = require("./routes/authRoutes")
@@ -10,6 +11,7 @@ dbConnect()
 
 const app = express()
 
+app.use(cors());
 // Middleware
 app.use(express.json())
 
