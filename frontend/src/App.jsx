d3190c5cfc2b5efed29d8aead44c08ac9977f516
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 03:51:31 +0530

    feat: add Login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
 
       <Route path="/" element= {<Home />}/>
+      <Route path="/login" element= {<Login />}/>
 
       </Routes>
     </Router>
