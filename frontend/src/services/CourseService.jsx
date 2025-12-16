82b2cb915853be33026bcc12c2f22b2b304a5e4b
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 06:07:09 +0530

    feat(course): add getAllAvailableCourses service

---
+++ frontend/src/services/CourseService.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { authHeader } from "./AuthService";
+
+const backendApiUrl = import.meta.env.VITE_BACKEND_API_URL
+
+export const allAvailableCourses = async() => {
+
+    try{
+        const response = await axios.get(
+            `${backendApiUrl}/course/publish/all`, {
+                headers: authHeader()   // Attach jwt token
+            }
+        )
+        
+        if (response.data.count === 0) {
+            console.warn("No courses available.");
+        }
+
+        return response.data.data
+    }
+    catch(error){
+        // Set backend error
+        const message = 
+            error.response?.data?.message ||
+            error.message ||
+            "Failed to fetch courses, Try again later";
+
+        throw new Error(message);
+    }
+}
+
