ab877f95537b1378c251fc2a494b0750b998b08e
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 07:00:27 +0530

    feat(course): add getAllEnrolledCourses by student service

--- frontend/src/services/CourseService.jsx
+++ frontend/src/services/CourseService.jsx
@@ -29,3 +29,28 @@ export const allAvailableCourses = async() => {
     }
 }
 
+export const getAllEnrolledCourses = async() => {
+
+    try{
+        const response = await axios.get(
+            `${backendApiUrl}/enrollment/student/courses`, {
+                headers: authHeader()   // Attach jwt token
+            }
+        )
+
+        if (response.data.count === 0) {
+            console.warn("No courses available.");
+        }
+
+        return response.data.data
+    }
+    catch(error){
+        // Set backend error
+        const message = 
+            error.response?.data?.message ||
+            error.message ||
+            "Failed to fetch enrolles courses, Try again later";
+
+        throw new Error(message);
+    }
+}
