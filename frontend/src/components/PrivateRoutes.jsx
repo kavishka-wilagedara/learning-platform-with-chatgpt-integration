2fb9ab6f9cdbbffdd94ddbf29d970015707a07da
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-16 07:34:57 +0530

    feat(auth): routes protected

---
+++ frontend/src/components/PrivateRoutes.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
+
+const PrivateRoute = ({ children, roles = [] }) => {
+  const { user } = useContext(AuthContext)
+
+  if (!user) return <Navigate to="/login" />
+
+  if (roles.length && !roles.includes(user.role)) {
+    return <Navigate to="/login" />
+  }
+
+  return children
+}
+
+export default PrivateRoute
