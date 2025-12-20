3849d6547b94ddc2937ca877e120c27d4f4328da
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-12-20 18:59:01 +0530

    feat(style): add custom toggle button for side bar expand and collapase

--- frontend/src/components/InsructorLayout.jsx
+++ frontend/src/components/InsructorLayout.jsx
@@ -1,12 +1,15 @@
 import { Link, Outlet, useNavigate } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
 import toast from "react-hot-toast"
+import { GoSidebarExpand, GoSidebarCollapse } from "react-icons/go";
+import { LiaSignOutAltSolid } from "react-icons/lia";
 
 const InstructorLayout = () => {
   const { user, logout } = useContext(AuthContext)
   console.log("user", user)
   const navigate = useNavigate()
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(true)
 
   const handleLogout = () => {
     logout();
@@ -16,32 +19,64 @@ const InstructorLayout = () => {
 
   return (
     <div className="flex h-screen">
+      
       {/* Left sidebar */}
-      <aside className="w-75 bg-gray-700 text-white flex flex-col">
-        <div className="p-8 font-bold text-xl border-b border-white">
-          Welcome, {user.firstname}
+      <aside 
+        className={`bg-gray-700 text-white flex flex-col transition-all duration-300 ${
+            sidebarIsOpen ? "w-72" : "w-20"
+          }`
+        }
+      >
+        <div
+          className="flex items-center justify-end p-4 cursor-pointer hover:bg-gray-600"
+          onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+        >
+          {sidebarIsOpen ? (
+            <GoSidebarExpand className="w-6 h-6"/>
+          ):(
+            <GoSidebarCollapse className="w-6 h-6"/>
+          )}
+
         </div>
+
+        {sidebarIsOpen && (
+          <div className="p-8 font-bold text-xl border-b border-white">
+            Welcome, {user.firstname}
+          </div>
+        )}
+
         <nav className="flex-1 flex flex-col p-4 space-y-2">
 
             <Link
                 to="/all"
-                className="px-3 py-2 rounded hover:bg-white hover:text-black"
+                className={`px-3 py-2 rounded hover:bg-white hover:text-black ${
+                  !sidebarIsOpen && "text-md text-center"
+                  }`
+                }
             >
-                View posted courses
+                {sidebarIsOpen ? "View posted courses" : "All"}
             </Link>
 
             <Link
                 to="/create"
-                className="px-3 py-2 rounded hover:bg-white hover:text-black"
+                className={`px-3 py-2 rounded hover:bg-white hover:text-black ${
+                  !sidebarIsOpen && "text-md text-center"
+                  }`
+                }
             >
-                Post the course
+                {sidebarIsOpen ? "Post the course" : "Post"}
             </Link>
           
             <button
-            onClick={handleLogout}
-            className="mt-auto px-3 py-2 rounded bg-red-500 hover:bg-red-600"
+              onClick={handleLogout}
+              className={`mt-auto px-3 py-2 rounded bg-red-500 hover:bg-red-600 ${
+                !sidebarIsOpen && "text- text-center"
+                }`
+              }
             >
-                Logout
+                {sidebarIsOpen ? "Log out" : 
+                  <LiaSignOutAltSolid className="w-6 h-6"/>
+                }
             </button>
         </nav>
       </aside>
